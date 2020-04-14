2fea131b41486dbc8a9de45fbd84ea716b024420
glptrst <giulio.pietroiusti@gmail.com>
2020-04-15 01:46:11 +0200

    Render chat.

--- public/chatty.js
+++ public/chatty.js
@@ -1,13 +1,45 @@
 "use strict";
 
-(() =>{
-  const HOST = location.origin.replace(/^http/, 'ws');
-  const ws = new WebSocket(HOST);
+(() => {
 
-  ws.onopen = () => {
-    console.log('WebSocket Client Connected');
+  let username;
+
+  document.getElementsByTagName('body')[0].addEventListener("keyup", (event) => {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      document.getElementById('usernameInput').style.display = 'none';
+      startChat(username);
+    }
+  });
+
+  function startChat() {
+    renderChat();
+
+    const HOST = location.origin.replace(/^http/, 'ws');
+    const ws = new WebSocket(HOST);
+
+    ws.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    ws.onmessage = function(e) {
+      //TODO
+    };
   };
-  ws.onmessage = function(e) {
-    //TODO
-  };
+
+  function renderChat() {
+    let body = document.getElementsByTagName('body')[0];
+    let ul = document.createElement('ul');
+    ul.id = 'messages';
+    body.appendChild(ul);
+
+    let form = document.createElement('form');
+    form.action = '';
+    let input = document.createElement('input');
+    input.id = 'm';
+    input.autocomplete = 'off';
+    form.appendChild(input);
+    body.appendChild(form);
+    input.focus();
+  }
+
 })();
